c28cca85f4fd767175f450f3cd9f52186dc234ce
khingcode <khingissupreme@gmail.com>
2024-05-25 01:07:23 +0800

    removed stuff to make use of github pages

--- giftbox/script.js
+++
@@ -1,9 +0,0 @@
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-}
-function togglePopup1(){
-    document.getElementById("popup-2").classList.toggle("active");
-}
-function togglePopup2(){
-    document.getElementById("popup-3").classList.toggle("active");
-}
